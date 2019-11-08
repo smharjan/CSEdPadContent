44592034359516bf7380313599eb897fdb36e13b
hmashrique <47196008+hmashrique@users.noreply.github.com>
2019-11-08 13:36:48 -0600

    Update NestedSwitchStatement.java

--- topics/Switch/NestedSwitchStatement.java
+++ topics/Switch/NestedSwitchStatement.java
@@ -10,15 +10,16 @@
 public class NestedSwitchStatement {
 
     public static void main(String[] args) {
-        /**
-         @comment(First,we define year and Branch value for checking nested
+        
+      /**
+         @comment (First,we define year and Branch value for checking nested
          switch statement.)
          */
         String branch = "CSE"; // Try different values for Branch and year to see how Nested Switch works.
         int year = 2;
         
         /**
-          @comment(Start of switch statement which checks the year value and
+          @comment (Start of switch statement which checks the year value and
           switches to different cases if matches.)
          */
         switch (year) {
@@ -29,7 +30,7 @@ public class NestedSwitchStatement {
             case 1:
                 System.out.println("elective courses : Advance english, Algebra");
                 /**
-                 @comment( After the print statement executes,break command causes execution to jump to the end of switch block. If this
+                 @comment (After the print statement executes,break command causes execution to jump to the end of switch block. If this
                   break was missing, it would execute every case block.)
                  */
                 break;
@@ -37,18 +38,18 @@ public class NestedSwitchStatement {
             case 2:
                 switch (branch) 
                     /**
-                   @comment(Since year value equals 2, Nested switch
+                   @comment (Since year value equals 2, Nested switch
                     statement starts executing.)
                     */
                 {
                     /**
-                     @comment(If Branch matches any case, then it begins
+                     @comment (If Branch matches any case, then it begins
                       execution on the next line..)
                      */
                     case "CSE":
                         System.out.println("elective courses : Machine Learning, Big Data");
                         /**
-                          @comment( After the print statement executes,break command causes execution to jump to the end of switch block.
+                          @comment (After the print statement executes,break command causes execution to jump to the end of switch block.
                          If this break was missing, it would execute every case block.)
                          */
                         break;
@@ -57,7 +58,7 @@ public class NestedSwitchStatement {
                         System.out.println("elective courses : Antenna Engineering");
                         break;
                     /**
-                     @comment(In the scenario where no cases matches for Branch value, the default statement is executed and it
+                     @comment (In the scenario where no cases matches for Branch value, the default statement is executed and it
                        prints out "Elective courses : Optimization".)
                      */
                     default:
