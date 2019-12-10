6e90f6e8bcb2e9e8f0530ea567bce1ddee607f00
Lasang Jimba Tamang <zimbabiru@gmail.com>
2019-12-10 12:12:41 -0600

    Create Rectangle.java
    demonstrate how to create object and access its method and instance variable in java

---
+++ topics/objects/Rectangle.java
@@ -0,0 +1,45 @@
+/**
+ @author (Lasang Tamang)
+ @topic (Objects)
+ @title (object creation and accessing methods)
+ @goalDescription (demonstrate how to create object and access its method and instance variable in java)
+ @source ()
+ @output (
+    height : 8.0
+    width : 12.0
+ )
+*/
+public class Rectangle {
+    private double height;
+	private double width;
+	
+	public double getHeight(){
+	    return height; 
+	}
+	
+	public double getWidth(){
+	    return width; 
+	}
+	public Rectangle(double height, double width){
+       this.height = height;
+       this.width = width;
+	}
+ 	public void magnify (int ratio) {
+  	    height = height * ratio;
+        width = width * ratio;
+	}
+	
+	
+	public static void main(String[] args) {
+	    //step 1 : Create Object 
+		Rectangle myBox = new Rectangle(4, 6);
+		
+		//step 2 :  Call class method magnify that alters value of height and width
+        myBox.magnify(2);
+        
+        // step 3: Access instance variables
+        System.out.println("height : "+myBox.getHeight());
+        System.out.println("width : "+myBox.width);
+    }
+
+}
