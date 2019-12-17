e77b688ff82a34bfee92cfd6d8c5b0b8cd2f2dfa
pritiOli <ollipriti@gmail.com>
2019-12-17 10:19:51 -0600

    for loops added

---
+++ topics/For loops/JCountDown.java
@@ -0,0 +1,17 @@
+ /**
+ @author(Priti Oli)
+ @topic(For Loops)
+ @title(JCountDown)
+ @goalDescription(Construct a program that counts down from 10 to 1 )
+ @source()
+ */
+public class JCountDown {
+    public static void main(String[] args) {
+//        step 1: declare a variable counter, assign counter=10 and initialise a for loop to iterate
+//        until the counter equals 0
+       for(int counter=10;counter>0;counter--){
+//           step 2: display the value of counter
+           System.out.println("counter = " + counter);
+       }
+    }
+}
