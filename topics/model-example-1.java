7832b6a1b5b14cf3425891e451278d63a251471b
Lasang Jimba Tamang <zimbabiru@gmail.com>
2020-01-30 14:43:24 -0600

    Create model-example-1.java

---
+++ topics/model-example-1.java
@@ -0,0 +1,42 @@
+/**
+ @author (Lasang Tamang)
+ @chapter (Arrays)
+ @topics (declaring array variables, Adding element of array, processing array)
+ @goalDescription(This program demonstrates how to create array, intialize them and print the values of arrays)
+ @source (Here we keep the URL if there exist any source )
+ @output (0,1,2,3,4,5,6,7,8,9)
+*/
+public class BasicArrayExample3{
+
+	public static void main(String[] args) {
+		
+		/*
+		logical_step_1: Declare variable size and intialize value 10
+		*/
+		int size = 10;
+		
+		/*
+		logical_step_2: Creates array mynumber of size 10)
+		execution_detail: Creates an array of size 10 of integer type.
+		*/
+		int[] myNumber = new int[10];	
+		
+		/*
+		logical_step_3: Add array elements 0 to 9 in array myNumber
+		execution_detail: The length of array myNumber is 10. The loop starts at for i = 0 and loops until i=9. 
+		For each value of i, array mynumber is intialized to value of  i.The new values of array myNumber is 0,1,2,3,4,5,6,7,8,9
+		*/
+		for(int i = 0; i < myNumber.length; i++) {	/*@stm_comment: loop 10 times, start at 0 and iterate until i is less than 10*/		
+			myNumber[i] = i;	/*@stm_comment: Insert the value of i for arry myNumber at index postion i*/			
+		}				
+		
+		/*
+		logical_step_4: Display the value of array mynumber
+		execution_detail: The length of myNumber is 10. The loop starts at for i = 0 and loops until i=9. 
+		For each value of i, the value of myNumber at index postion i is printed. The program prints all the value of array myNumber.
+		*/
+		for(int i = 0; i < myNumber.length; i++) {	/*@stm_comment(loop 10 times, start at 0 and iterate until i is less than 10*/		
+			System.out.println(myNumber[i]);	/*@stm_comment(Insert the value of i for arry myNumber at index postion i*/			
+		}
+	}
+}
