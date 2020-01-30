89b77883b5569c0cc671ee25e20133c0267fb4e6
Lasang Jimba Tamang <zimbabiru@gmail.com>
2020-01-30 14:45:01 -0600

    Update model-example-1.java

--- topics/model-example-1.java
+++ topics/model-example-1.java
@@ -24,10 +24,11 @@ public class BasicArrayExample3{
 		/*
 		logical_step_3: Add array elements 0 to 9 in array myNumber
 		execution_detail: The length of array myNumber is 10. The loop starts at for i = 0 and loops until i=9. 
-		For each value of i, array mynumber is intialized to value of  i.The new values of array myNumber is 0,1,2,3,4,5,6,7,8,9
+		For each value of i, array mynumber is intialized to value of  i.The new values of array myNumber
+		is 0,1,2,3,4,5,6,7,8,9
 		*/
-		for(int i = 0; i < myNumber.length; i++) {	/*@stm_comment: loop 10 times, start at 0 and iterate until i is less than 10*/		
-			myNumber[i] = i;	/*@stm_comment: Insert the value of i for arry myNumber at index postion i*/			
+		for(int i = 0; i < myNumber.length; i++) {/*@stm_comment: loop 10 times, start at 0 and iterate until i is less than 10*/		
+			myNumber[i] = i;/*@stm_comment: Insert the value of i for arry myNumber at index postion i*/			
 		}				
 		
 		/*
@@ -35,8 +36,8 @@ public class BasicArrayExample3{
 		execution_detail: The length of myNumber is 10. The loop starts at for i = 0 and loops until i=9. 
 		For each value of i, the value of myNumber at index postion i is printed. The program prints all the value of array myNumber.
 		*/
-		for(int i = 0; i < myNumber.length; i++) {	/*@stm_comment(loop 10 times, start at 0 and iterate until i is less than 10*/		
-			System.out.println(myNumber[i]);	/*@stm_comment(Insert the value of i for arry myNumber at index postion i*/			
+		for(int i = 0; i < myNumber.length; i++) { /*@stm_comment(loop 10 times, start at 0 and iterate until i is less than 10*/		
+			System.out.println(myNumber[i]); /*@stm_comment(Insert the value of i for arry myNumber at index postion i*/			
 		}
 	}
 }
