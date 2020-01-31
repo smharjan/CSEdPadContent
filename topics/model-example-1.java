9bbd9dbd4b1e4fd16e09c1d9f051e8f5cd1595c1
Lasang Jimba Tamang <zimbabiru@gmail.com>
2020-01-31 11:40:31 -0600

    Update model-example-1.java

--- topics/model-example-1.java
+++ topics/model-example-1.java
@@ -23,7 +23,7 @@ public class BasicArrayExample3{
 		question_2: how many times does the loop executes for?
 		question_3What are the final values of myNumber ?
 		
-		answer_1The length of array myNumber is 10.
+		answer_1:The length of array myNumber is 10.
 		answer_2:The loop executes for 9 times.
 		answer_3:The new values of array myNumber is 0,1,2,3,4,5,6,7,8,9
 		*/
