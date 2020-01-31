f6eefe1e7e680ebb3b4eb2769f7d692de01f8b1a
Lasang Jimba Tamang <zimbabiru@gmail.com>
2020-01-31 11:01:23 -0600

    Update model-example-1.java

--- topics/model-example-1.java
+++ topics/model-example-1.java
@@ -17,7 +17,9 @@ public class BasicArrayExample3{
 		
 		
 		/*
-		logical_step_3: Add array elements 0 to 9 in array myNumber
+		logical_step_2: Add array elements 0 to 9 in array myNumber
+		Questions:
+		
 		execution_detail: The length of array myNumber is 10. The loop starts at for i = 0 and loops until i=9. 
 		For each value of i, array mynumber is intialized to value of  i.The new values of array myNumber
 		is 0,1,2,3,4,5,6,7,8,9
