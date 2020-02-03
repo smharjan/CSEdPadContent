5ae7cc874683e2e764d93cccd4f77dca46aa6eeb
Lasang Jimba Tamang <zimbabiru@gmail.com>
2020-02-02 20:25:06 -0600

    Update model-example-1.java

--- topics/model-example-1.java
+++ topics/model-example-1.java
@@ -9,36 +9,49 @@
 public class BasicArrayExample3{
 
 	public static void main(String[] args) {
-		
+
 		/*
-		logical_step_1: Declare variable size and intialize value 10
+		logical_step_1: declare array variable called myNumber of type integer of size 10 
+		question_1: What variables are declared?
+		answer_1: The variable integer size and array myNumber are declared.
+		question_2: The value of size is 10. Now, what is the size of array mynumber ?
+		answer 2: The size of the array myNumber is 10.
+		question 3: what does the following code block do?
+		answer_3: declare array variable called myNumber of type integer of size 10 
+	
 		*/
 		int size = 10;
-		int[] myNumber = new int[size];	
-		
-		
+		int[] myNumber = new int[size]; /*@stm_comment: declare an array variable called myNumber of type integer and size 10 and allocate memory for it*/
+
 		/*
-		logical_step_2: Add array elements 0 to 9 in array myNumber
-		Questions_1: What is the length of array? 
-		question_2: how many times does the loop executes for?
-		question_3What are the final values of myNumber ?
-		
-		answer_1:The length of array myNumber is 10.
-		answer_2:The loop executes for 9 times.
-		answer_3:The new values of array myNumber is 0,1,2,3,4,5,6,7,8,9
+		logical_step_2: Assign the values from 1 to 10 to the elements of array myNumber
+		question_1: What is the start and stopping codnition for for loop?
+		answer_1: For loop starts at i = 0 and stops when i<10.
+		question_2: waht does the code inside for loop doing?
+		answer_2: The code assigns each value of i during iteration to array myNumber at index postion i
+		question_3: What does the following code block do?
+		answer_3: Assign the values from 1 to 10 to the elements of array myNumber
 		*/
-		
-		for(int i = 0; i < myNumber.length; i++) {/*@stm_comment: loop 10 times, start at 0 and iterate until i is less than 10*/		
-			myNumber[i] = i;/*@stm_comment: Insert the value of i for arry myNumber at index postion i*/			
-		}				
-		
+		/*@stm_comment: the size of the array is given by myNumber.length which is 10*/
+		for(int i = 0; i < myNumber.length; i++) { 
+			/*@stm_comment: the i-th element of the array myNumber which 
+			is denoted my myNumber[i] is assigned the value of i*/
+			myNumber[i] = i; 
+		}
+
 		/*
-		logical_step_3: Display the value of array mynumber
-		execution_detail: The length of myNumber is 10. The loop starts at for i = 0 and loops until i=9. 
-		For each value of i, the value of myNumber at index postion i is printed. The program prints all the value of array myNumber.
+		logical_step_3: Display each values of the array myNumber
+		question_1: Do the for loop iterates over each value of array?
+		answer_1: Yes.
+		question_1: Once they iterates over each value of array myNumber, what does the it do inside for loop?
+		answer_1: for each value of i, it prints the value of myNumber at index postion i
+		question_3: What does the following code block do?
+		answer_3: Display each values of the array myNumber
 		*/
-		for(int i = 0; i < myNumber.length; i++) { /*@stm_comment: loop 10 times, start at 0 and iterate until i is less than 10*/		
-			System.out.println(myNumber[i]); /*@stm_comment: Insert the value of i for arry myNumber at index postion i*/			
+		/*@stm_comment: process or iterates over each element of the array myNumber  */
+		for(int i = 0; i < myNumber.length; i++) { 
+			/*@stm_comment: myNumber[i] access the value of myNumber at index  position  i*/
+			System.out.println(myNumber[i]); 
 		}
 	}
 }
