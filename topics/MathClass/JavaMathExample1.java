bc0cef6a0c185f1de5f24c0986c1261dc91a0ef9
ZeyadAlshaikh <alshaikh.zeyad@gmail.com>
2020-02-10 23:00:02 -0500

    updating the code to match the new guidelines

--- topics/MathClass/JavaMathExample1.java
+++ topics/MathClass/JavaMathExample1.java
@@ -1,9 +1,16 @@
 /*
-@author(Zeyad Alshaikh)
-@topic(Math class)
-@title(JavaMathExample1.java)
-@goalDescription(Demonstrate the use of Math class)
-@source(https://www.javatpoint.com/java-math)
+author: Zeyad Alshaikh
+topics: Math class
+subtopics: using Math.max, Math.sqrt, Math.pow, Math.log, Math.log10, Math.log1p,Math.exp and Math.expm1
+goalDescription: This program demonstrates how to use Math class.
+source: https://www.javatpoint.com/java-math
+output:Maximum number of x and y is: 28.0
+Power of x and y is: 614656.0
+Logarithm of x is: 3.332204510175204
+log10 of x is: 1.4471580313422192
+log1p of x is: 3.367295829986474
+exp of x is: 1.446257064291475E12
+expm1 of x is: 1.446257064290475E12
 */
 
 public class JavaMathExample1
@@ -13,46 +20,28 @@ public class JavaMathExample1
         double x = 28;
         double y = 4;
 
-        /**
-         @comment( return the maximum of two numbers)
-         */
-        System.out.println("Maximum number of x and y is: " +Math.max(x, y));
+        /*@stm_comment: Math.max(x,y) return the maximum of x and y */
+        System.out.println("Maximum number of x and y is: " +Math.max(x,y));
 
-        /**
- @comment( return the square root of y )
- */
+        /*@stm_comment: Math.sqrt(y) returns the square root of y* /
         System.out.println("Square root of y is: " + Math.sqrt(y));
 
-        /**
- @comment(returns 28 power of 4 i.e. 28*28*28*28 )
- */
-        System.out.println("Power of x and y is: " + Math.pow(x, y));
+        /*@stm_comment: Math.pow(x, y) returns the power of x to y */
+        System.out.println("Power of x and y is: " + Math.pow(x,y));
 
-        /**
- @comment( return the logarithm of given value)
- */
+        /*@stm_comment: Math.log(x) returns the natural logarithm of x */
         System.out.println("Logarithm of x is: " + Math.log(x));
-        System.out.println("Logarithm of y is: " + Math.log(y));
 
-        /**
- @comment( return the logarithm of given value when base is 10)
- */
+        /*@stm_comment: Math.log10(x) returns the base 10 logarithm of x */
         System.out.println("log10 of x is: " + Math.log10(x));
-        System.out.println("log10 of y is: " + Math.log10(y));
 
-        /**
- @comment( return the log of x + 1 )
- */
+        /*@stm_comment: Math.log1p(x) returns the natural logarithm of 1+x */
         System.out.println("log1p of x is: " +Math.log1p(x));
 
-        /**
- @comment( return a power of 2   )
- */
+        /*@stm_comment: Math.exp(x) returns e (the base of the natural logarithms) to the power of x */
         System.out.println("exp of a is: " +Math.exp(x));
 
-        /**
- @comment( return (a power of 2)-1 )
- */
-        System.out.println("expm1 of a is: " +Math.expm1(x));
+        /*@stm_comment: Math.expm1(x) returns e (the base of the natural logarithms) to the power of (x-1) */
+        System.out.println("expm1 of x is: " +Math.expm1(x));
     }
 }
