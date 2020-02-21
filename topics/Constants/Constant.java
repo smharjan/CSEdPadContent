7aeb59093911f97aac63dea44e63847009e0f520
hmashrique <hmshrque@memphis.edu>
2020-02-21 16:24:03 -0600

    "Changes made in VScode"

--- topics/Constants/Constant.java
+++ topics/Constants/Constant.java
@@ -3,14 +3,17 @@
  @author(Hasan Mashrique)
  @topic(Constants)
  @title(PIconstant)
- @goalDescription(Construct a program that uses a constant value and prints it out)
+ @goalDescription(Construct a program that creates a constant variable to hold number Pi and print it to the console. 
+)
  @source()
 */
 public class Constant {
 
     public static void main(String args[]) {
     
-     /**
+     // NEED TO ADD QUESTIONS as a story, ALSO LOGICAL STEPS
+     
+        /**
     @comment (We declare a double variable which stores the value of PI. We store it as final since in java constants are declared as final.)
     */
         final double PI =3.1416;  
--- topics/DoWhile/DoWhile.java
+++ topics/DoWhile/DoWhile.java
@@ -1,4 +1,4 @@
-package javapedagogy;
+//package javapedagogy;
 
 /**
  @author : Hasan Mashrique
@@ -14,16 +14,20 @@ public class DoWhile {
     public static void main(String[] args) {
 
         /*
+        logical_step_1: Declare variable and assign an initial value to check how many candy a child can have. 
+        logical_step_details: First we declare a integer variable and a value to it. We also declare another variable to check how many more 
+        candy a child can have.
         question_1: what variables are declared?
-        answer_1: an integer variable 'i' is declared.
+        answer_1: an integer variable i to set how many candies is eaten and a count variable to check how many more it can have.
         question_2: what does the following code block do?
-        answer_2: It declares a integer variable and assigns a variable to it.
+        answer_2: It declares two integer variables and assigns values to them. One for candies already eaten and another for how many more 
+        one can eat (limit is 12).
         
         */
 
-       /*@stm_comment: declare an integer variable 'i' and assign value to it.  */
+       /*@stm_comment: declare integer variables 'i' and 'count' assign values to them.  */
         int i = 8; 
-        
+        int count = 0;
         
        /*
         question_1: what does the code do inside the do while loop?
@@ -37,11 +41,15 @@ public class DoWhile {
         */  
 
         do {
-            /*@stm_comment: prints the value of i */
-            System.out.println(i); 
+            /*@stm_comment: increments value of i */
             i++;
-        } while (i <= 10); /*@stm_comment: if i is >10 , it will still print once since the condition is checked after the while 
-        execution once.*/
+            /*@stm_comment: counts hoy mane more it can eat */
+            count++;
+            
+            
+        } while (i <= 12); /*@stm_comment: if i is >10 , it will still print once since the condition is checked after the while execution once.*/
+        
+        System.out.println("Candies left to eat: " + (count-1));
     }
 
 }
