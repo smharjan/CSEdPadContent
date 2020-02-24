c74da51dd301f2a1c660b8e153eabed9c24ad0ff
Jeevan Chapagain <cjeevaniam@gmail.com>
2020-02-24 11:50:48 -0600

    aprimitive data type examples

--- topics/5.Primitive Data Types/Primitive1.java
+++ topics/5.Primitive Data Types/Primitive1.java
@@ -1,16 +1,47 @@
-/***
-@author(Jeevan Chapagain)
-@topic(Primitive Data types example)
-@title(Primitive1.java)
-@goalDescrption(JAVA program to show example of primitive data types)
-***/
+/**
+author(Jeevan Chapagain)
+topic(Primitive Data Types )
+title(Primitive1.java)
+goalDescrption(JAVA program to illustrate use of primitive data types)
+ @source (https://www.w3schools.com/java/java_data_types.asp)
 
+output 34
+	   100
+	   15000000000
+ 
+*/
 
 public class ByteDemo {
+
 	public static void main(String[] args) {
+
+		/*
+		logical_step_1: Decalaring primitive data type.
+		question_1: what are the data types used in this code?
+		answer_1: byte, short and float are the data types used here.
+		question_2: what is the type of the data types used here?
+		answer_2: the used data types here are of integer types.
+		*/
+
 		byte num = 34;  
-		short level = 1000;  
-		float num  = 3.46f; 
+		/*stm_comment:  byte data type is declared using byte keyword which can store whole number from -127 to 127*/
+
+		short maxNum = 1000;  
+		/*stm_comment:  short data type is declared using short  keyword which can store whole number from -32768 to 32767*/
+
+
+		long myNum  = 15000000000L; 
+		/*stm_comment:  long data type is declared using long keyword  and you should end your value with L. Used when int can not store value.
+		*/
+
+
+		System.out.println(num);
+		/*stm_comment: printing out the result of byte data type. */
+
+    	System.out.println(maxNum);
+
+    	System.out.println(myNum);
+
 	}
 }
  
