1593661fccb23fe754b66b611b01df6de0bc115e
jordanbarriap <jordanbarriap@gmail.com>
2020-02-25 14:12:18 -0500

    Update RecursiveIsPalindrome.java

--- topics/Recursion/RecursiveIsPalindrome.java
+++ topics/Recursion/RecursiveIsPalindrome.java
@@ -20,8 +20,8 @@ public class RecursiveIsPalindrome(){
     
      public boolean isPalindrome(String str) {
             /*
-            logical_step_1: the recursion base case that returns true when there is only one character left, where the function always return true
-            question_1: what case for recursive algorithms is this if statement representing?
+            logical_step_1: the recursion base case that returns true when there is only one or none characters left, where the function always return true
+            question_1: what type of case for recursive algorithms is this if statement representing?
             answer_1: one of the recursive "base case"
              */
              if (str.length() <= 1) {
@@ -31,21 +31,20 @@ public class RecursiveIsPalindrome(){
              else{
                 /*
                 logical_step_2: the recursion base case that returns false when the first and last characters do not match
-                question_1: what case for recursive algorithms is this if statement representing?
+                question_1: what type of case for recursive algorithms is this if statement representing?
                 answer_1: one of the recursive "base case"
                 */
                  if (str.charAt(0) != str.charAt(str.length() - 1)) {
-                     // Base case when this method knows to return false because the first and last characters do not match.
+                     //stmt_code: Base case when this method knows to return false because the first and last characters do not match.
                      return false;
                  }
                  /*
                 logical_step_3: the recursion base case is whenever there is only one character on both sides, where the function checks if the two characters are the same or not
-                question_1: what case for recursive algorithms is this if statement representing?
+                question_1: what type of case for recursive algorithms is this if statement representing?
                 answer_1: the recursive "base case"
                 */
                  else{
-                     // The first and last characters are equal so check if the shorter
-                     // string--a simpler version of this problem--is a palindrome.
+                     //stmt_comment: The first and last characters are equal so check if the shorter string--a simpler version of this problem--is a palindrome.
                      return isPalindrome(str.substring(1, str.length() - 1));
                 }
             }
