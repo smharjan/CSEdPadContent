ef00555c8c61493ffcec69a356cd4cfd89fff967
jordanbarriap <jordanbarriap@gmail.com>
2020-02-25 13:51:49 -0500

    Update RecursiveIsPalindrome.java

--- topics/Recursion/RecursiveIsPalindrome.java
+++ topics/Recursion/RecursiveIsPalindrome.java
@@ -2,12 +2,14 @@
 author : Jordan Barria-Pineda
 topics : Recursion
 subtopics: recursive method calling, recursion base case
-goalDescription: This program computes Fibonacci numbers using a recursive method
-source: Big Java Book (3rd edition), page 603 (only difference is that original input n was 50)
+goalDescription: This program checks a string is palindrome or not (first half of the string is the same as the second half). 
+source: Arizona State University online book https://www2.cs.arizona.edu/~mercer/Book/PrinterFriendly/19Tall.pdf
+output: Yes
 **/
 
-public static void main(String args[]) 
-    { 
+public class RecursiveIsPalindrome(){
+
+    public static void main(String args[]){ 
         String str = "geeg"; 
   
         if (isPalindrome(str)) 
@@ -16,28 +18,36 @@ public static void main(String args[])
             System.out.println("No"); 
     } 
     
-// A recursive function that check a str(s..e) is palindrome or not. 
-    static boolean isPalRec(String str, int s, int e){ 
-    
-        /*
-        logical_step_1: the recursion base case is whenever there is only one character on both sides, where the function checks if the two characters are the same or not
-        question_1: what case for recursive algorithms is this if statement representing?
-        answer_1: the recursive "base case"
-        */
-        if (s == e) 
-            return true; 
- 
-        if ((str.charAt(s)) != (str.charAt(e))) 
-            return false; 
-  
-        /*
-        logical_step_2: the recursive calls happen whenever the string has more than 2 characters
-              question_1: what case for recursive algorithms is this if statement representing?
-              answer_1: the recursive calls case
-        */
-        /* smt_comment: If there are more than two characters, check if middle substring is also palindrome or not. 
-        if (s < e + 1) 
-            return isPalRec(str, s + 1, e - 1); 
-  
-        return true; 
-    } 
+     public boolean isPalindrome(String str) {
+          /*
+            logical_step_1: the recursion base case is whenever there is only one character on both sides, where the function checks if the two characters are the same or not
+            question_1: what case for recursive algorithms is this if statement representing?
+            answer_1: the recursive "base case"
+           */
+             if (str.length() <= 1) {
+                 //stm_comment: Base case when this method knows to return true.
+                 return true;
+             }
+             else{
+                /*
+                logical_step_2: the recursion base case is whenever there is only one character on both sides, where the function checks if the two characters are the same or not
+                question_1: what case for recursive algorithms is this if statement representing?
+                answer_1: the recursive "base case"
+                */
+                 if (str.charAt(0) != str.charAt(str.length() - 1)) {
+                     // Base case when this method knows to return false because the first and last characters do not match.
+                     return false;
+                 /*
+                logical_step_3: the recursion base case is whenever there is only one character on both sides, where the function checks if the two characters are the same or not
+                question_1: what case for recursive algorithms is this if statement representing?
+                answer_1: the recursive "base case"
+                */
+                 }else {
+                     // The first and last characters are equal so check if the shorter
+                     // string--a simpler version of this problem--is a palindrome.
+                     return isPalindrome(str.substring(1, str.length() - 1));
+                }
+            }
+        }
+    }
+}
