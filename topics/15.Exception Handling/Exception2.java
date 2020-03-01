72773e59441a592a0b10168441d38d2a661046c5
Jeevan Chapagain <cjeevaniam@gmail.com>
2020-03-01 10:56:59 -0600

    Exception Handling Annotated

--- topics/15.Exception Handling/Exception2.java
+++ topics/15.Exception Handling/Exception2.java
@@ -1,25 +1,68 @@
 /***
-@author(Jeevan Chapagain)
-@topic(Exception Handling)
-@title(Exception2.java)
-@goalDescrption(JAVA program to handle string out of bounds exception)
+author:Jeevan Chapagain
+topic:Exception Handling
+title:Exception2.java
+goalDescrption: JAVA program to handle Null Pointer exception
+output: NullPointerException
 ***/
  
 Public class Main {
  public static void main(String[] args) {
-   //code inside try block runs if it is true
+
+  /*
+   logical_step1: Declare a try and catch statement to catch the exception occured during the execution of a program. Within the try block we create
+   variable of string data type and find out the which character is at index position 0. 
+   logical_step_details: Declare try and catch statement such that  the code inside try block will be executed and if any error is
+   occured it will be handled by the catch block.
+   question_1: what does the following code block do?
+   answer_1: Within a try block, find out the index position 0 of the character stored in variable x.   */ 
+
    try {
-         //Step 1 : Initialize a variable with null value
-           String x = null;
-         //Step 2 : Print out the char at index 0 which produces error since our variable is null
-           System.out.println(x.charAt(0));
+  /*stm_comment: Tries executing code, providing opportunity to catch and handle errors*/
+
+  /*
+  logical_step_1: Create a variable of string data type.
+  logical_step_details: Declare a variable empty of string data type with null value assigned to it.
+  question_1: What variable is declared?
+  answer_1: Variable empty is declared.
+
+  */
+
+           String empty = null;
+  /*stm_comment: declare a variable empty of data type String and assign null value to it.*/
+  
+  /*
+  logical_step_2: Find index position 0 of the character present in  the string variable empty.
+  logical_step_details: Search for the index position 0 of the character using charAt method.
+  question_2: How do you find the index position of the character?
+  answer_2: Index position of the character can be found out using the method charAt().
+
+  */
+
+           System.out.println(empty.charAt(0));
+      /*stm_comment: printing out the character present ar index position 0 in the variable empty.*/
+
+
        }
+
+
+  /*
+  logical_step_3: Catch exceptions thrown by the previous operation, printing out the name of the exception.
+  logical_step_details: The catch block will catch any exceptions thrown by the code executed in the try block and says what exception is occured.
+  question_3: what does the following code block do?
+  answer_3: Catch Null pointer Exception thrown by the previous code of block in try block. 
+  */
     
-       // Step 3: catch keyword will handle the exception since we are using  null pointer we use NullPointerException
        catch(NullPointerException e) {
-         //Step 4: Print the exception
-           System.out.println("NullPointerException..");
+
+  /*stm_comment: NullPointer Exception is thrown when we try to access the index of a string which is null. */
+
+           System.out.println("NullPointerException");
+
+  /*stm_comment: Printing out what exception occured during the execution of the program */
+
        }
  }
+
 }
 
