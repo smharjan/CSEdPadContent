ff0c09e6498b19e6df802aad19248c4bbcb9d02f
priti_oli <ollipriti@gmail.com>
2020-03-02 11:51:37 -0600

    Update Leapyear.java

--- topics/6-Logical Operators/Leapyear.java
+++ topics/6-Logical Operators/Leapyear.java
@@ -17,11 +17,15 @@ public class Leapyear {
         
         /*
 		logical_step_1: use logical and conditional operators to check whether a year is leap year or not.
+		logical_step_details: using modulus operator check if the variable year is divisble by 4 or 400 and not divisible
+		by 100, using logical operator 'and' and 'or' , check if the conditions are satisfied using ternary operator and 
+		store the result in variable leapYear.
 		question_1: What is the initial value of the variable leapYear?
 		answer_1: the value of the variable leapYear is 'It's a leap Year'
 		question_2: What is the value of the variable leapyear if year=2021
 		answer_2: The value of the variable leapyear will be 'Not a Leap year!'
-		*/
+		
+	*/
 
         leapYear=(year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))?"It's a leap Year":"Not a Leap year!";
         System.out.println(leapYear);
