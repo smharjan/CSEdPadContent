90e6c0432a3a65eb3a97b96a9453090dbda7e244
priti_oli <ollipriti@gmail.com>
2020-03-02 12:18:13 -0600

    logical_step_details added

--- topics/For loops/CountDown.java
+++ topics/For loops/CountDown.java
@@ -21,17 +21,25 @@
 public class CountDown {
     public static void main(String[] args) {
 		/*
-		logical_step_1: Declare a for loop by initialising the value of the variable counter 
-		question_1: What is the initial value of the variable counter?
-		answer_1: the initial value of the variable counter is 10
-		question_2: What is the value of the variable counter after the for loop ends?
-		answer_2: The value of the variable counter is 0 when the loop ends
-		question_3: What will be the output of the program if the line ' for(int counter=10;counter>0;counter--){' is replaced by 'for(int counter=10;counter<0;counter--){'  ?
-		answer_3: the program will not display any output as the loop doesn't execute 
+		logical_step_1: Dispay the value of counter from 10 to 1 
+		logical_step_details: Declare a for loop by initialising the value of the variable counter to be 10 
+		and decrement the value of variable counter in each iteration and display the value of variable counter 
+		in each iteration.
+		question_1: What does the following block of code do?
+		answer_1: the code block declares a for loop by initialising the value of the variable counter to be 10 
+		and decrement the value of variable counter in each iteration and display the value of variable counter 
+		in each iteration.
+		question_2: What is the initial value of the variable counter?
+		answer_2 the initial value of the variable counter is 10
+		question_3: What is the value of the variable counter after the for loop ends?
+		answer_3: The value of the variable counter is 0 when the loop ends
+		question_4: What will be the output of the program if the line ' for(int counter=10;counter>0;counter--)
+		{' is replaced by 'for(int counter=10;counter<0;counter--){'  ?
+		answer_4: the program will not display any output as the loop doesn't execute 
 		*/
   
        for(int counter=10;counter>0;counter--){
-/*stm_comment: t display the value of the variable counter in each iteration */
+	/*stm_comment: display the value of the variable counter in each iteration */
            System.out.println("counter = " + counter);
        }
     }
