a963db5d3478e47a3d3cbf2effce8f032c36c9cf
smharjan <46822390+smharjan@users.noreply.github.com>
2020-03-03 12:40:04 -0600

    Updated OtherClass with "logical_step_details"

--- topics/Classes/OtherClass.java
+++ topics/Classes/OtherClass.java
@@ -11,27 +11,30 @@ class OtherClass {
   	public static void main(String[] args) {
 
 	  	/*
-	  	logical_step_2: Create an object of MyClass, we need to specify the class name, followed by the object name, and use the keyword new
-		question_2: What is the purpose of newObj1?
-		answer_2: newObj1 has a state and behavior of MyClass
+	  	logical_step_1 Create an object of MyClass, we need to specify the class name, followed by the object name, and use the keyword new
+		logical_step_details: Creating a object of class i.e., MyClass which is a separate JAVA file and in order to create the object of that class we need the class name at front then the object name where we have created one of that class i.e., newObj1. And also we need to use the keyword "new" 
+		question_1: What is the purpose of newObj1?
+		answer_1: newObj1 has a state and behavior of MyClass
 	    */
 
 	    MyClass newObj1 = new MyClass();
 	    System.out.println(newObj1.x);
 
 	    /*
-	  	logical_step_3: Call a static method from MyClass which needs class name in from of the static method name, i.e; here MyClass.staticMethod() calls the static method of MyClass
-		question_3: What is the purpose of this?
-		answer_3: It calls the static method of MyClass
+	  	logical_step_2: Call a static method from MyClass which needs class name in from of the static method name, i.e; here MyClass.staticMethod() calls the static method of MyClass
+		logical_step_details: Calls a static method from MyClass and performs the functions described inside that method
+		question_2: What is the purpose of this?
+		answer_2: It calls the static method of MyClass
 	    */
 
 	    /*stm_comment: Call the static method from MyClass */
 		MyClass.staticMethod();
 
 		/*
-	  	logical_step_4: Create an object of MyClass, we need to specify the class name, followed by the object name, and use the keyword new
-		question_4: What is the purpose of 2?
-		answer_4: newObj2 has a state and behavior of MyClass and calls the publicMethod
+	  	logical_step_3: Create an object of MyClass, we need to specify the class name, followed by the object name, and use the keyword new
+		logical_step_details: Creating a object of class i.e., MyClass and in order to create the object of that class we need the class name at front then the object name where we have created one of that class i.e., newObj2. And also we need to use the keyword "new" 
+		question_3: What is the purpose of 2?
+		answer_3: newObj2 has a state and behavior of MyClass and calls the publicMethod
 		*/
 
 		/*stm_comment: Create object newObj of MyClass */
