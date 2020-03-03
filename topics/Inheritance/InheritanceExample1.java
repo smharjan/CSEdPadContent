6b56439e5d3cea69c6cdcca57dd301c86140d04f
ZeyadAlshaikh <alshaikh.zeyad@gmail.com>
2020-03-03 00:31:37 -0500

    Update InheritanceExample1.java

--- topics/Inheritance/InheritanceExample1.java
+++ topics/Inheritance/InheritanceExample1.java
@@ -2,8 +2,8 @@
  author: Zeyad Alshaikh
  topics: Inheritance
  subtopics: Using Inheritance, method Override
- goalDescription: Demonisterate the inheritance concept by creating a superclass Bicycle with two public integer features gear and speed, and toString fuction.
- Then create subclass MountainBike that extends the Bicycle with public integer feature seatHeight and Override toString funciton.
+ goalDescription: Demonisterate the inheritance concept by creating a superclass Bicycle with two public integer features gear and speed, and toString function.
+ Then create subclass MountainBike that extends the Bicycle with public integer feature seatHeight and Override toString function.
  source: https://www.geeksforgeeks.org/inheritance-in-java/
  output: Number of gears are 3
 speed of bicycle is 100
@@ -22,7 +22,7 @@ class Bicycle
      question_2: What features are declared?
      answer_2: Public integer gear and speed.
      question_3: What is the scope of the public features?
-     answer_3: The socpe of the public features is anywhere.
+     answer_3: The scope of the public features is anywhere.
    */
 
   /*stmt_comment: declare public integer feature name gear */
@@ -32,11 +32,11 @@ class Bicycle
 
   /*
      logical_step_2: Declare a parameterized constructor for class Bicycle.
-     logical_step_details: Declare parameterized constructor that recieves two integer parameters gear and speed, and assign the variables' value to the features.
+     logical_step_details: Declare parameterized constructor that receives two integer parameters gear and speed, and assign the variables' value to the features.
      question_1: What does the following code block do?
-     answer_1: Declare parameterized constructor that recieves two integer parameters gear and speed, and assign the variables' value to the features.
+     answer_1: Declare parameterized constructor that receives two integer parameters gear and speed, and assign the variables' value to the features.
      question_2: What is the number and type of arguments the constructor accept?
-     answer_2: The constructor accept two integer parameters.
+     answer_2: The constructor accepts two integer parameters.
    */
   public Bicycle (int gear, int speed)
   {
@@ -47,30 +47,30 @@ class Bicycle
   }
 
   /*
-     logical_step_3: Declare a function that return well formated string.
-     logical_step_details: Declare a function the return a formated string that contains Number of gears are the value of gear feature and in new line  speed of bicycle is  the value of speed feature.
+     logical_step_3: Declare a function that returns well-formatted string.
+     logical_step_details: Declare a function the return a formated string that contains Number of gears are the value of gear feature and in new line speed of bicycle is the value of speed feature.
      question_1: What does the following code block do?
-     answer_1: Declare a function the return a formated string that contains Number of gears are the value of gear feature and in new line  speed of bicycle is  the value of speed feature.
+     answer_1: Declare a function the return a formated string that contains Number of gears are the value of gear feature and in new line speed of bicycle is the value of speed feature.
      question_2: What is the return value of this function?
-     answer_2: Number of gears are the value of gear feature and in new line  speed of bicycle is  the value of speed feature.
+     answer_2: Number of gears are the value of gear feature and in new line speed of bicycle is the value of speed feature.
    */
   public String toString ()
   {
-    /*stmt_comment: return the following sting: Number of gears are the value of gear feature and in new line  speed of bicycle is  the value of speed feature  */
+    /*stmt_comment: return the following string: Number of gears are the value of gear feature and in new line  speed of bicycle is  the value of speed feature  */
     return ("Number of gears are " + gear
 	    + "\n" + "speed of bicycle is " + speed);
   }
 }
 
 /*
-logical_step_4: Declare a subclass name MountainBike inherited from Bicycle.
+logical_step_4: Declare a subclass name MountainBike inherit from Bicycle.
 logical_step_details: The MountainBike extends the Bicycle class to inherite the features and functions.
 question_1: What does the following code block do?
-answer_1: Declare a subclass name MountainBike inherited from Bicycle.
+answer_1: Declare a subclass name MountainBike inherit from Bicycle.
 question_2: What does class MountainBike inherited from Bicycle?
 answer_2: The class MountainBike inherited the features gear and speed and all functions from Bicycle.
 */
-/*stmt_comment: Declare a class name MountainBike that inherite from Bicycle */
+/*stmt_comment: Declare a class name MountainBike that inherits from Bicycle */
 class MountainBike extends Bicycle
 {
 
@@ -83,16 +83,16 @@ class MountainBike extends Bicycle
      answer_2: gear, speed and seatHeight
    */
 
-  /*stmt_comment: declare public integer feature name seatHeight */
+  /*stmt_comment: Declare public integer feature name seatHeight */
   public int seatHeight;
 
   /*
      logical_step_6: Declare a parameterized constructor for class MountainBike.
-     logical_step_details: Declare parameterized constructor that recieves three integer parameters gear and speed and seatHeight. Then it call the constructor of Bicycle using the keyword super, and assing the vale of seatHeight to the feature seatHeight.
+     logical_step_details: Declare parameterized constructor that receives three integer parameters gear and speed and seatHeight. Then it calls the constructor of Bicycle using the keyword super and assigns the value of seatHeight to the feature seatHeight.
      question_1: What does the following code block do?
-     answer_1: Declare parameterized constructor that recieves three integer parameters gear and speed and seatHeight. Then it call the constructor of Bicycle using the keyword super, and assing the vale of seatHeight to the feature seatHeight.
+     answer_1: Declare parameterized constructor that receives three integer parameters gear and speed and seatHeight. Then it calls the constructor of Bicycle using the keyword super and assigns the vale of seatHeight to the feature seatHeight.
      question_2: What is the number and type of arguments the constructor accept?
-     answer_2: The constructor accept three integer parameters.
+     answer_2: The constructor accepts three integer parameters.
    */
   public MountainBike (int gear, int speed, int seatHeight)
   {
@@ -106,13 +106,13 @@ class MountainBike extends Bicycle
      logical_step_7: Declare an Override function to the toString function in Bicycle class.
      logical_step_details: Declare a function the return a formated string that contains a string from toString function in Bicycle class and in a new line seat height is the value of seatHeight feature.
      question_1: What does the following code block do?
-     answer_1: Declare a function the return a formated string that contains a string from toString function in Bicycle class and in a new line seat height is the value of seatHeight feature.
+     answer_1: Declare a function the return a formated string that contains a string from toString function in Bicycle class, and in a new line seat height is the value of seatHeight feature.
      question_2: What is the return value of this function?
-     answer_2: Number of gears are the value of gear feature, in new line  speed of bicycle is  the value of speed feature then in new line seat height is the value of seatHeight.
+     answer_2: Number of gears are the value of gear feature, in new line  speed of bicycle is  the value of speed feature, then in new line seat height is the value of seatHeight.
    */
    @Override public String toString ()
   {
-    /*stmt_comment: eturn the following sting: Number of gears are the value of gear feature, in new line  speed of bicycle is  the value of speed feature then in new line seat height is the value of seatHeight */
+    /*stmt_comment: return the following string: Number of gears are the value of gear feature, in new line  speed of bicycle is  the value of speed feature then in new line seat height is the value of seatHeight */
     return (super.toString () + "\nseat height is " + seatHeight);
   }
 
@@ -132,14 +132,14 @@ public class InheritanceExample1
        question_2: What are the values of gear, speed and seatHeight?
        answer_2: 3,100,25
        question_3: what is the output of the following block?
-       answer_3:Number of gears are 3
+       answer_3: Number of gears are 3
        speed of bicycle is 100
        seat height is 25
      */
 
     /*stmt_comment: Declare an object mb from the class MountainBike and initialize it with the following values 3,100, and 25 */
     MountainBike mb = new MountainBike (3, 100, 25);
-    /*stmt_comment:print the returned value from toString function in MountainBike class */
+    /*stmt_comment:Print the returned value from toString function in MountainBike class */
       System.out.println (mb.toString ());
 
   }
