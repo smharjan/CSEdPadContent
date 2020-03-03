a91b50978ea2e6cb97046128eaaca7c6d84d27d2
ZeyadAlshaikh <alshaikh.zeyad@gmail.com>
2020-03-03 00:36:29 -0500

    Update InheritanceExample2.java

--- topics/Inheritance/InheritanceExample2.java
+++ topics/Inheritance/InheritanceExample2.java
@@ -13,10 +13,10 @@ class Animal
 {
 
   /*
-     logical_step_1: Declare a function that prints eating....
-     logical_step_details: Declare a function name eat where this function print eating... to the screen.
+     logical_step_1: Declare a function name eat....
+     logical_step_details: Declare a function name eat that prints eating... to the screen.
      question_1: What does the following code block do?
-     answer_1: print eating... to the screen
+     answer_1: Print eating... to the screen
    */
   void eat ()
   {
@@ -27,19 +27,19 @@ class Animal
 
 /*
 logical_step_2: Declare a subclass name Cat inherited from Animal.
-logical_step_details: The Cat class extends the Animal class to inherite the features and functions.
+logical_step_details: The Cat class extends the Animal class to inherit the features and functions.
 question_1: What does the following code block do?
 answer_1: Declare a subclass name Cat inherited from Animal.
-question_2: What does class Cat inherited from Animal?
+question_2: What does class Cat inherit from Animal?
 answer_2: The function eat.
 */
-/*stmt_comment: Declare a class name Cat that inherite from Animal */
+/*stmt_comment: Declare a class name Cat that inherit from Animal */
 class Cat extends Animal
 {
 
   /*
-     logical_step_1: Declare a function that prints meowing....
-     logical_step_details: Declare a function name meow where this function print meowing... to the screen.
+     logical_step_3: Declare a function that prints meowing....
+     logical_step_details: Declare a function name meow where this function prints meowing... to the screen.
      question_1: What does the following code block do?
      answer_1: print meowing... to the screen
    */
@@ -56,12 +56,12 @@ class InheritanceExample2
   {
 
     /*
-       logical_step_1: Declare an object c from the class Cat and initialize it by calling the default constructor. Then it call meow and eat functions.
+       logical_step_4: Declare an object c from the class Cat and initialize it by calling the default constructor. Then it calls meow and eat functions.
        logical_step_details: Declare an object c from the class Cat and initialize it by calling the default constructor.
        question_1: What does the following code block do?
        answer_1: Create an object from the class Cat and call meow and eat function.
        question_2: what is the output of the following block?
-       answer_2:meowing...
+       answer_2: meowing...
        eating...
      */
 
