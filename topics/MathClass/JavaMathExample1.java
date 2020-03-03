bb5885cd3c4143c9d31adaaef6405fcc75a396f3
ZeyadAlshaikh <alshaikh.zeyad@gmail.com>
2020-03-03 00:43:38 -0500

    Update JavaMathExample1.java

--- topics/MathClass/JavaMathExample1.java
+++ topics/MathClass/JavaMathExample1.java
@@ -2,7 +2,7 @@
  author: Zeyad Alshaikh
  topics: Math class
  subtopics: using Math.sqrt, Math.pow, Math.PI, Math.acos
- goalDescription: Construct a program that finds the length of hypotenuse in of a right triangle by using Pythagoras theorem then finds the angles of that triangle.
+ goalDescription: Construct a program that finds the length of the hypotenuse in of a right triangle by using the Pythagoras theorem then finds the angles of that triangle.
  source: http://javauk.blogspot.com/2013/07/java-hypotenuse-right-triangle.html
  output: The length of the hypotenuse (c) is: 10.0
 Angles are A = 36.86989764584401, B = 53.13010235415598, C = 90
@@ -14,7 +14,7 @@ public class MathExample1{
 
     /*
        logical_step_1: Declare double variables called a, b, and c to represent each side of the triangle and assign a to 6 and b to 8.
-       logical_step_details: First we declare double variables a and b and a value to it. We also declare another a double variable c to hold the length of hypotenuse.
+       logical_step_details: First, we declare double variables a and b and a value to it. We also declare another a double variable c to hold the length of the hypotenuse.
        question_1: What does the following code block do?
        answer_1: Declare double variables called a, b, and c to represent each side of the triangle and assign a to 6 and b to 8.
        question_2: What variables are declared?
@@ -23,14 +23,14 @@ public class MathExample1{
        answer_3: The value of a is 6, b is 8 and c is 0.0d
      */
 
-    /*stmt_comment: initialize the variables a to 6, b to 8 and c to 0.0d */
+    /*stmt_comment: Initialize the variables a to 6, b to 8 and c to 0.0d */
     double a = 6, b = 8, c;
 
     /*
-       logical_step_2: Calculate the length of the hypotenuse (c) by using Pythagoras theorem.
+       logical_step_2: Calculate the length of the hypotenuse (c) by using the Pythagoras theorem.
        logical_step_details: calculate the length of the hypotenuse (c) by using Math.sqrt and Math.pow functions.
-       question_1:What does the following code block do?
-       answer_1: Calculate the length of the hypotenuse (c) by using Pythagoras theorem where c = squar root of power 2 of a plus the power 2 of b.
+       question_1: What does the following code block do?
+       answer_1: Calculate the length of the hypotenuse (c) by using Pythagoras theorem where c = square root of power 2 of a plus the power 2 of b.
        question_2: What the value of c?
        answer_2: 10
      */
@@ -40,42 +40,42 @@ public class MathExample1{
     /*
        logical_step_3: Declare two double variables A and B and assign it the calculated value the angles of the right triangle.
        logical_step_details: First, find the angle A by applying the arc cosine to the formula (b * b + c * c - a * a) / (2 * b * c).
-       Then convert to convert into degrees by multiply it by 180 and devide it by PI.
+       Then convert to convert into degrees by multiplying it by 180 and divide it by PI.
        After that, find the angle B by applying the arc cosine to the formula (a * a + c * c - b * b) / (2 * a * c) .
-       Then convert to convert into degrees by multiply it by 180 and devide it by PI.
+       Then convert to convert into degrees by multiplying it by 180 and divide it by PI.
        question_1: What does the following code block do?
        answer_1: First, find the angle A by applying the arc cosine to the formula (b * b + c * c - a * a) / (2 * b * c).
-       Then convert to convert into degrees by multiply it by 180 and devide it by PI.
+       Then convert to convert into degrees by multiplying it by 180 and divide it by PI.
        After that, find the angle B by applying the arc cosine to the formula (a * a + c * c - b * b) / (2 * a * c) .
-       Then convert to convert into degrees by multiply it by 180 and devide it by PI.
+       Then convert to convert into degrees by multiplying it by 180 and divide it by PI.
        question_2: What the value of A?
        answer_2: 36.86
        question_3: What the value of B?
        answer_3: 53.13
      */
-    /*stmt_comment: initialize the variables A to the value of Math.acos((b * b + c * c - a * a) / (2 * b * c)) */
+    /*stmt_comment: Initialize the variables A to the value of Math.acos((b * b + c * c - a * a) / (2 * b * c)) */
     double A = Math.acos ((b * b + c * c - a * a) / (2 * b * c));
     /*stmt_comment: Assing the variable A to the value of (A * 180) / Math.PI */
       A = (A * 180) / Math.PI;
 
-    /*stmt_comment: initialize the variables B to the value of Math.acos((a * a + c * c - b * b) / (2 * a * c)) */
+    /*stmt_comment: Initialize the variables B to the value of Math.acos((a * a + c * c - b * b) / (2 * a * c)) */
     double B = Math.acos ((a * a + c * c - b * b) / (2 * a * c));
     /*stmt_comment: Assing the variable B to the value of (B * 180) / Math.PI */
       B = (B * 180) / Math.PI;
 
     /*
        logical_step_4: Print the length of the hypotenuse (c) and the angles of A and B.
-       logical_step_details: First, it print the length of hypotenuse (c) then it print the angles of A and B in new line.
+       logical_step_details: First, it prints the length of the hypotenuse (c), then it prints the angles of A and B in a new line.
        question_1: question_1: What does the following code block do?
-       answer_1: First, it print the length of hypotenuse (c) then print the angles of A and B in new line.
-       question_2: what the ouput of the following block?
-       answer_2:The length of the hypotenuse (c) is: 10.0
+       answer_1: First, it prints the length of the hypotenuse (c), then prints the angles of A and B in a new line.
+       question_2: What the output of the following block?
+       answer_2: The length of the hypotenuse (c) is: 10.0
        Angles are A = 36.86989764584401, B = 53.13010235415598, C = 90
      */
 
     /*stmt_comment: Print The length of the hypotenuse (c) is: the value of the c variable */
       System.out.println ("The length of the hypotenuse (c) is: " + c);
-    /*stmt_comment: Print TAngles are A =  the value of the A variable, B = the value of the B variable, C = 90 */
+    /*stmt_comment: Print Angles are A =  the value of the A variable, B = the value of the B variable, C = 90 */
       System.out.println ("Angles are A = " + A + ", B = " + B + ", C = " +
 			  90);
 
