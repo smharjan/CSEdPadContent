15c2716e3fb3d73d0cb84d5e833337d3bcb66e42
Lasang Jimba Tamang <zimbabiru@gmail.com>
2020-03-03 12:28:32 -0600

    Update ArrayListEx2.java

--- topics/arrayList/ArrayListEx2.java
+++ topics/arrayList/ArrayListEx2.java
@@ -9,12 +9,12 @@
 apple watch
 umbrella
 */
-import java.util.*; 
-import java.util.ArrayList; 
+import java.util.*;
+import java.util.ArrayList;
 
 public class Main {
-	public static void main(String[] args) {
-		/*
+ public static void main(String[] args) {
+  /*
     logical_step_1: Initalzes an arraylist wishList with first 3 items
     logical_step_details: Creates and initialzes an arraylist wishList which element are of type string and add iphone 10, apple watch and go pro
     question_1: What does the following code block do?
@@ -24,17 +24,17 @@ public class Main {
     question_3: What element are added to travelList ?
     answer_3: iphone 10, apple watch and go pro	
  */
-		
-                          
-    ArrayList<String> wishList = new ArrayList<String>() { 
-            { 
-                add("iphone 10"); 
-                add("apple watch"); 
-                add("go pro"); 
-            } 
-        }; 
-	
-	/*
+
+
+  ArrayList < String > wishList = new ArrayList < String > () {
+   {
+    add("iphone 10");
+    add("apple watch");
+    add("go pro");
+   }
+  };
+
+  /*
   	logical_step_2: Modifies wish list
   	logical_step_details: Modies iphone 1o to iphone 11, removes go pro and add umbrella
   	question_1: What does the following code block do?
@@ -46,19 +46,19 @@ public class Main {
   	question_3:waht item is added ?
   	answer_3: go pro
   	*/
-  	    wishList.set(0,"iphone 11");
-	    wishList.remove("go pro");
-	    wishList.add("umbrella");
-	    
-	    /*
+  wishList.set(0, "iphone 11");
+  wishList.remove("go pro");
+  wishList.add("umbrella");
+
+  /*
   	logical_step_2: Displays wishlist
   	logical_step_details: Itreates thorugh each item of wishlist and displays them
   	question_1: What does the following code block do?
   	answer_1: Displays wishlist
   	*/
-	    for(String item:wishList)
-         System.out.println(item);
-	   
+  for (String item: wishList)
+   System.out.println(item);
 
-	}
+
+ }
 }
