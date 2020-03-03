5820857aab31eb7fbc113795a67a4ffce844fbc1
smharjan <46822390+smharjan@users.noreply.github.com>
2020-03-03 12:38:29 -0600

    Updated Class2 with "logical_step_details"

--- topics/Classes/Class2.java
+++ topics/Classes/Class2.java
@@ -9,6 +9,7 @@ public class Class2 {
 
 	/*
     logical_step_1: Create static and public method
+    logical_step_details: Creating two type of methods that are static and public, where static method can be called without creating objects of Class2 but for the public method to be called we need to create object of Class2
     question_1: What is static method?
     answer_1: Static method can be accessed without creating an object of the class
     question_2: What is public method?
@@ -33,6 +34,7 @@ public class Class2 {
 
 		/*
 	  	logical_step_2: Create an object of Class2, we need to specify the class name, followed by the object name, and use the keyword new
+    	logical_step_details: Creating a object of class i.e., Class2 and in order to create the object of that class we need the class name at front then the object name where we have created one of that class i.e., newObj. And also we need to use the keyword "new" 
     	question_3: What is the purpose of newObj?
     	answer_3: newObj has a state and behavior of Class2 and calls the publicMethod
     	*/
